807959780a91c5c8f9f4d753ad6979f106351b47
Ka Cheng Ieong <ieongkacheng@gmail.com>
2022-02-17 16:45:04 -0500

    Create two more interface class and modify the car class

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
+from engines import Engine
+from battery import Battery
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(Serviceable):
+    def __init__(self):
+        self.engine = Engine()
+        self.battery = Battery()
 
-    @abstractmethod
     def needs_service(self):
         pass
