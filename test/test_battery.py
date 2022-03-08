27c5084156cc02fa4a24a80dd759136baf15a88f
Ka Cheng Ieong <ieongkacheng@gmail.com>
2022-03-08 12:29:09 -0500

    Upgrade the Spindler battery to be service 3 year instead of 2

--- test/test_battery.py
+++ test/test_battery.py
@@ -22,11 +22,11 @@ class TestNubbin(unittest.TestCase):
 class TestSpindler(unittest.TestCase):
     def test_spindler_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
         battery = SpindlerBattery(last_service_date, today)
         self.assertTrue(battery.needs_service())
     def test_spindler_should_not_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 1)
+        last_service_date = today.replace(year = today.year - 2)
         battery = SpindlerBattery(last_service_date, today)
         self.assertFalse(battery.needs_service())
